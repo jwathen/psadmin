e0cb2626d92e3183a525bae5cfea6e761dea8c9e
John Wathen <john.s.wathen@gmail.com>
2017-07-10 21:08:52 -0700

    module 8

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import {Router,browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
 import routes from './routes';
+import configureStore from './store/configureStore';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('app'));
